7aa5bc7336217596c0378c61c236e9eab12d1fd3
Facundo Ferro <facundo@cargofive.com>
2021-11-09 00:43:46 -0300

    server change port

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.get('/',(req,res)=>{
     res.send('Hello World with express new version!')
 }) 
 
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(8080);
+console.log('Server on port 8080')
